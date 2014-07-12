bb2f3eb3fb26996360df98368169eaedf486ee9c
jakedemian <jakedemian@gmail.com>
2014-07-12 19:51:29 -0400

    finished base "development process" section

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-function handleWindowResize()
-{
-	
-}
-
 $(document).ready(function()
 {
-	handleWindowResize();
 
-	$(window).resize(function()
-    {
-        handleWindowResize();
-    });
 
 
 });
