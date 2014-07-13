413de77f5c84c89081a3bae37b2155ccd2a6d340
jakedemian <jakedemian@gmail.com>
2014-07-13 00:31:25 -0400

    base site complete: read comments for todo
    TODO:
    
    -clean up code.  it's very messy.
    -make sure mobile is fully functional
    -add some other soft animations
    -work on the aesthetics of the page
    -overlay something on the main pic

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+function goToByScroll(id)
+{
+    id = id.replace("link", "");
+
+	if(id != "teamScroll")
+	{
+	    $('html,body').stop().animate({
+	        scrollTop: $("."+id).offset().top-130},
+	        'slow');
+	}
+	else
+	{
+		$('html,body').stop().animate({
+			scrollTop: $("."+id).offset().top-80},
+			'slow');
+	}
+}
+
 $(document).ready(function()
 {
-
+	$('#about').click(function()
+    {
+        goToByScroll("aboutScroll");
+		console.log("lksjdf");
+    });
+    $('#team').click(function()
+    {
+        goToByScroll("teamScroll");
+    });
+    $('#process').click(function()
+    {
+        goToByScroll("processScroll");
+    });
+	$('#contact').click(function()
+    {
+        goToByScroll("contactScroll");
+    });
 
 
 });
